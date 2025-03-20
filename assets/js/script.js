be5949dc89aaea98354906529efaf1dd5a24d289
Julie Crotzer <jcrotzerrn@aol.com>
2025-03-20 11:48:14 -0500

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,86 @@
+const slider = document.querySelector("#slider");
+const lengthValue = document.querySelector(".generator__length-value");
+const checkboxes = document.querySelectorAll(".generator__checkbox");
+const generateBtn = document.querySelector(".generator__generate-btn");
+const passwordDisplay = document.querySelector(".generator-result p");
+const copyBtn = document.querySelector(".generator__copy-btn");
+const copiedText = document.querySelector(".generator__copied-text");
+const strengthText = document.querySelector(".generator__strength-text");
+const strengthBars = document.querySelectorAll(".generator__strength-bar");
+
+slider.addEventListener("input", handleSliderInput);
+generateBtn.addEventListener("click", generatePassword);
+copyBtn.addEventListener("click", copyToClipboard);
+
+function handleSliderInput() {
+  lengthValue.textContent = this.value;
+  updateSliderColor(this.value);
+}
+
+function updateSliderColor(value) {
+  const breakpoint = (value / 20) * 100;
+  slider.style.background = `linear-gradient(90deg, #a4ffaf ${breakpoint}%, #18171f ${breakpoint}%)`;
+}
+
+function generatePassword(event) {
+  event.preventDefault();
+  const length = slider.value;
+  const options = {
+    uppercase: document.querySelector("input[name='uppercase']").checked,
+    lowercase: document.querySelector("input[name='lowercase']").checked,
+    numbers: document.querySelector("input[name='numbers']").checked,
+    symbols: document.querySelector("input[name='symbols']").checked,
+  };
+
+  const charset = buildCharset(options);
+  if (!charset) {
+    passwordDisplay.textContent = "Select options!";
+    return;
+  }
+
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    password += charset.charAt(Math.floor(Math.random() * charset.length));
+  }
+
+  passwordDisplay.textContent = password;
+  updateStrengthIndicator(password);
+}
+
+function buildCharset({ uppercase, lowercase, numbers, symbols }) {
+  let charset = "";
+  if (uppercase) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (lowercase) charset += "abcdefghijklmnopqrstuvwxyz";
+  if (numbers) charset += "0123456789";
+  if (symbols) charset += "!@#$%^&*()_-+=<>?/";
+  return charset;
+}
+
+function copyToClipboard() {
+  const textarea = document.createElement("textarea");
+  textarea.value = passwordDisplay.textContent;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  document.body.removeChild(textarea);
+
+  copiedText.classList.remove("generator__copied-text--hidden");
+  setTimeout(
+    () => copiedText.classList.add("generator__copied-text--hidden"),
+    2000
+  );
+}
+
+function updateStrengthIndicator(password) {
+  let strength = 0;
+  if (password.match(/[A-Z]/)) strength++;
+  if (password.match(/[a-z]/)) strength++;
+  if (password.match(/[0-9]/)) strength++;
+  if (password.match(/[^a-zA-Z0-9]/)) strength++;
+
+  strengthText.textContent =
+    ["Too Weak", "Weak", "Medium", "Strong"][strength - 1] || "Too Weak";
+  strengthBars.forEach((bar, index) => {
+    bar.dataset.level = index < strength ? strength : "";
+  });
+}
---
+++ password-generator-app/README.md
@@ -0,0 +1,85 @@
+# Frontend Mentor - Password generator app solution
+
+This is a solution to the [Password generator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/password-generator-app-Mr8CLycqjh). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Generate a password based on the selected inclusion options
+- Copy the generated password to the computer's clipboard
+- See a strength rating for their generated password
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+
+### Screenshot
+
+[Password Generator Screenshot](/assets/images/Screenshot%202025-03-20%20105750.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://github.com/JCrotzer/password-generator.git)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+
+### What I learned
+
+Use this section to recap over some of your major learnings while working through this project. Writing these out and providing code samples of areas you want to highlight is a great way to reinforce your own knowledge.
+
+To see how you can add code snippets, see below:
+
+```
+<h1>Some CSS code I'm proud of</h1>
+```
+
+```css
+.generator__slider-input::-webkit-slider-thumb {
+  appearance: none;
+  width: 28px;
+  height: 28px;
+  border-radius: 50%;
+  background-color: var(--white);
+  cursor: pointer;
+}
+```
+
+````css
+.generator__slider-input::-moz-range-thumb {
+  appearance: none;
+  width: 28px;
+  height: 28px;
+  border-radius: 50%;
+  background-color: var(--white);
+  cursor: pointer;
+}
+```css .generator__slider-input::-webkit-slider-thumb:active,
+.generator__slider-input::-moz-range-thumb {
+  background-color: var(--very_dark_gray);
+  border: 2px solid var(--neon-green);
+}
+````
+
+## Author
+
+- Frontend Mentor - [@JCrotzer](https://www.frontendmentor.io/profile/JCrotzer)
